1330ad055c3d3d2ac32358ed6658ea33be1d35e2
[dyad] <karantikoo965@gmail.com>
2025-10-03 14:07:21 +0530

    [dyad] add Dyad component tagger

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import dyadComponentTagger from '@dyad-sh/react-vite-component-tagger';
 
 
 // https://vitejs.dev/config/
@@ -9,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [
+  plugins: [dyadComponentTagger(), 
     react(),
     
   ].filter(Boolean),
