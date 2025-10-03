ef2f86b017ee022705b5a573a0a857b2acdb7b06
[dyad] <karantikoo965@gmail.com>
2025-10-03 14:11:06 +0530

    [dyad] Refactored Indian main courses into modular files and added new Indian beverages - wrote 8 file(s)

--- src/data/foods/indian.ts
+++ src/data/foods/indian.ts
@@ -3,10 +3,12 @@ import { indianMainCourses } from "./indian/main-courses";
 import { indianBreakfasts } from "./indian/breakfasts";
 import { indianSnacksDesserts } from "./indian/snacks-desserts";
 import { southIndianFoods } from "./indian/south-indian";
+import { indianBeverages } from "./indian/beverages"; // New import
 
 export const indianFoods: FoodItem[] = [
   ...indianMainCourses,
   ...indianBreakfasts,
   ...indianSnacksDesserts,
   ...southIndianFoods,
+  ...indianBeverages, // Include beverages
 ];
