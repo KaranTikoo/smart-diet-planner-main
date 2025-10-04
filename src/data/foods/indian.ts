620e9a52d133fcc1a930328598594a3fcd478551
[dyad] <karantikoo965@gmail.com>
2025-10-04 10:38:45 +0530

    [dyad] wrote 2 file(s)

--- src/data/foods/indian.ts
+++ src/data/foods/indian.ts
@@ -3,7 +3,7 @@ import { indianMainCourses } from "./indian/main-courses";
 import { indianBreakfasts } from "./indian/breakfasts";
 import { indianSnacksDesserts } from "./indian/snacks-desserts";
 import { southIndianFoods } from "./indian/south-indian";
-import { indianBeverages } from "./beverages"; // Corrected import path
+import { indianBeverages } from "./indian/beverages"; // Corrected import path
 
 export const indianFoods: FoodItem[] = [
   ...indianMainCourses,
