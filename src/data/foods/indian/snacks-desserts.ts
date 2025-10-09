7a49c1c43ea576c18a8ce87c4b0e35391e91ffe5
[dyad] <karantikoo965@gmail.com>
2025-10-09 14:59:33 +0530

    [dyad] Updated favicon, replaced external image URLs with a local placeholder, and removed duplicate Indian food data. - wrote 26 file(s), deleted 15 file(s)

--- src/data/foods/indian/snacks-desserts.ts
+++
@@ -1,8 +0,0 @@
-import { FoodItem } from "@/types/food";
-import { indianSavorySnacks } from "./snacks-desserts/snacks";
-import { indianDesserts } from "./snacks-desserts/desserts";
-
-export const indianSnacksDesserts: FoodItem[] = [
-  ...indianSavorySnacks,
-  ...indianDesserts,
-];
